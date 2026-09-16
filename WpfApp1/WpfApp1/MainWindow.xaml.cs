3752d828a8ade61a163247b0892083ef7f6aaa99
owner <owner@C301-52>
2026-09-16 11:49:00 +0800

    加入專案檔案。

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // 生成九九乘法表
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    sb.Append($"{j}x{i}={i * j,2}   ");
+                }
+                sb.AppendLine();
+            }
+
+            // 將結果寫入 XAML 的 resultTextBox 文字方塊中
+            resultTextBox.Text = sb.ToString();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+    }
+}
